d10738e78ffb333c80abeeb1e95fccbd7e4db8f3
yjk101 <113004114+yjk101@users.noreply.github.com>
2024-06-11 12:19:09 +0900

    Update max.py

--- max.py
+++ max.py
@@ -60,19 +60,19 @@ def maxPop(total_df):
     st.markdown("<hr>", unsafe_allow_html=True)
     
     fig, ax = plt.subplots()
-    ax.bar(avg_population_head['자치구명'], avg_population_head['총생활인구수'])
+    ax.bar(avg_population_head['자치구명'], avg_population_head['총생활인구수'], fontproperties=font_name)
     ax.set_ylabel("평균 생활인구수")
     ax.set_xlabel("자치구")
-    ax.set_title(f'{acc_year}년 {selected_month} {selected_time} 평균 생활인구수가 가장 많은 자치구')
+    ax.set_title(f'{acc_year}년 {selected_month} {selected_time} 평균 생활인구수가 가장 많은 자치구', fontproperties=font_name)
     st.pyplot(fig)
     
     st.markdown("<hr>", unsafe_allow_html=True)
     
     fig, ax = plt.subplots()
-    ax.bar(avg_population_tail['자치구명'], avg_population_tail['총생활인구수'])
+    ax.bar(avg_population_tail['자치구명'], avg_population_tail['총생활인구수'], fontproperties=font_name)
     ax.set_ylabel("평균 생활인구수")
     ax.set_xlabel("자치구")
-    ax.set_title(f'{acc_year}년 {selected_month} {selected_time} 평균 생활인구수가 가장 적은 자치구')
+    ax.set_title(f'{acc_year}년 {selected_month} {selected_time} 평균 생활인구수가 가장 적은 자치구', fontproperties=font_name)
     st.pyplot(fig)
     
 
