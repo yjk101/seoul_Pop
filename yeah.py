895642634f3e03d866a212c169ad955886a50f89
yjk101 <113004114+yjk101@users.noreply.github.com>
2024-06-11 13:05:13 +0900

    Update yeah.py

--- yeah.py
+++ yeah.py
@@ -13,7 +13,7 @@ import matplotlib.font_manager as fm
 from seViz import code_to_name
 
 # 한글 폰트 설정
-path = 'NanumGothic.ttf'
+path = 'NanumGothic-Bold.ttf'
 fontprop = fm.FontProperties(fname=path, size=12)  
 
 def yeah():
