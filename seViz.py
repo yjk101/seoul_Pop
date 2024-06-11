63924c712189fea684d18f9d6e34e4fedaffee12
yjk101 <113004114+yjk101@users.noreply.github.com>
2024-06-11 13:14:52 +0900

    Update seViz.py

--- seViz.py
+++ seViz.py
@@ -13,9 +13,8 @@ from millify import prettify
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
-font_path = 'NanumGothic-Bold.ttf'  # 나눔고딕 폰트 경로
-font_name = fm.FontProperties(fname=font_path, size=10)# .get_name()
-# plt.rc('font', family=font_name)
+path = 'NanumGothic-Bold.ttf'
+fontprop = fm.FontProperties(fname=path, size=12)
 
 def code_to_name(code):
     district_code_name_mapping = {
@@ -90,9 +89,9 @@ def seViz(total_df):
     
     fig, ax = plt.subplots()
     ax.plot(daily_pop['day'], daily_pop['총생활인구수']) # , marker='o'
-    ax.set_title(f"{code_to_name(sgg_seViz)} 생활인구수 변화", fontproperties=font_name)
-    ax.set_xlabel("날짜", fontproperties=font_name)
-    ax.set_ylabel("생활인구수", fontproperties=font_name)
+    ax.set_title(f"{code_to_name(sgg_seViz)} 생활인구수 변화", fontproperties=fontprop)
+    ax.set_xlabel("날짜", fontproperties=fontprop)
+    ax.set_ylabel("생활인구수", fontproperties=fontprop)
     # ax.set_ylim(0, max_population)
     ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))  # y 축 단위 설정
     st.pyplot(fig)
@@ -104,9 +103,9 @@ def seViz(total_df):
     
     fig, ax = plt.subplots()
     ax.plot(filtered_day['시간대구분'], filtered_day['총생활인구수']) # , marker='o'
-    ax.set_title(f"{code_to_name(sgg_seViz)}의 {selected_month} {selected_day}일 시간대별 생활인구수", fontproperties=font_name)
-    ax.set_xlabel("시간", fontproperties=font_name)
-    ax.set_ylabel("생활인구수", fontproperties=font_name)
+    ax.set_title(f"{code_to_name(sgg_seViz)}의 {selected_month} {selected_day}일 시간대별 생활인구수", fontproperties=fontprop)
+    ax.set_xlabel("시간", fontproperties=fontprop)
+    ax.set_ylabel("생활인구수", fontproperties=fontprop)
     # ax.set_ylim(0, max_population)
     ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))  # y 축 단위 설정
     st.pyplot(fig)
